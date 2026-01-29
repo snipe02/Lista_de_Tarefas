845a782939d1c1cfab53e271b67d48c3641b4ba9
lucas_mestre <108341139+snipe02@users.noreply.github.com>
2026-01-28 23:01:09 -0300

    Update firebase.js
    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,3 @@
-// ============================================
-// CONFIGURAÇÃO DO FIREBASE
-// ============================================
 const firebaseConfig = {
     apiKey: "AIzaSyBeb-WqRwrDVdoY4hTASMHUNxE6-5oFV_E",
     authDomain: "tarefa-e19fd.firebaseapp.com",
@@ -9,4 +6,5 @@ const firebaseConfig = {
     messagingSenderId: "621346171627",
     appId: "1:621346171627:web:c0ac6a47d18d3be2452cae",
     measurementId: "G-LD1L0VZ5NP"
+
 };
